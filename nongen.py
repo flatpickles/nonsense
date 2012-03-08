993459b287bb74b4df51ca5ef656b2250342f3da
Matt Nichols <matt@mattnichols.net>
2012-03-08 00:07:50 -0500

    nicer error message

--- nongen.py
+++ nongen.py
@@ -114,7 +114,7 @@ except:
 word_list = getWords(target, "wordlist.txt")
 
 if word_list == None:
-	print "ERROR"
+	print "The Nonsense Generator can't resolve " + target
 else:
 	poem = ''
 	for line in genLines(word_list, num_lines, num_words):
