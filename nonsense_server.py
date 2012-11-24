6034747cfeaa93f9bfb91596a961f2e72c005197
Matt Nichols <matt@mattnichols.net>
2012-11-23 23:32:56 -0500

    adding path for jsonp requests

--- nonsense_server.py
+++ nonsense_server.py
@@ -1,5 +1,6 @@
 from nongen import getPoem
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from functools import wraps
 app = Flask(__name__)
 
 template = """
@@ -9,6 +10,20 @@ template = """
   </center></body>
 </html>
 """
+
+# allow jsonP
+# mostly via https://gist.github.com/1094140
+def jsonp(f):
+  global app
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+    callback = request.args.get('callback', False)
+    if callback:
+      content = str(callback) + '(' + str(f(*args,**kwargs).data) + ')'
+      return app.response_class(content, mimetype='application/javascript')
+    else:
+      return f(*args, **kwargs)
+  return decorated_function
 
 @app.route("/", methods=['GET'])
 def withParams():
@@ -24,6 +39,14 @@ def shortPoem():
 @app.route("/default")
 def defaultPoem():
   return getPoem("http://eliotswasteland.tripod.com/twl.html", 5, 7, True)
+
+@app.route("/json")
+@jsonp
+def getJSON():
+  url = request.args.get('url', 'http://eliotswasteland.tripod.com/twl.html')
+  lines = request.args.get('lines', '5')
+  words = request.args.get('words', '7')
+  return jsonify({'poem': getPoem(url, int(lines), int(words), True)})
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
